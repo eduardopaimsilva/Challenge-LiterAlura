f93fa196a39a719d0932e222d6d0dff3cbd07cfe
eduardo paim silva <edups.ww.com@hotmail.com>
2024-06-12 16:39:11 -0300

    Consumindo dados de séries

---
+++ src/main/java/br/com/alura/LiterAlura/service/ConsumoApi.java
@@ -0,0 +1,30 @@
+package br.com.alura.LiterAlura.service;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumoApi {
+
+    public String obterDados(String livro) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(livro))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        String json = response.body();
+        return json;
+    }
+}
